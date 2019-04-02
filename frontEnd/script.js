733de7ba56719279b39df348d448246d74c5dcf1
jatinkmr <kumar.jatin873@gmail.com>
2019-04-02 14:44:38 +0530

    jQuery Script Check

---
+++ frontEnd/script.js
@@ -0,0 +1,18 @@
+$(() => {
+	// console.log('Hello');
+
+	let msglst = $('#msgList');
+	let sndbtn = $('#sndmsg');
+	let msgbx = $('#msgBox');
+	let lgnbtn = $('#login');
+
+	lgnbtn.click(() => { 
+		// console.log('Button Clicked');		
+	});
+
+	sndbtn.click(() => { 
+		// console.log('Message Send');
+	});
+
+	
+});
